d91c40b8d64625b0a2ba9dc361e78fae0f4bb78a
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-04-19 13:03:11 +0530

    feat: generate predictions function
    - added utility function to get difference between dates
    - added scrolll area incase to avoid overflow

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,3 @@
 export const BASE_URL = "https://api.openweathermap.org/data/2.5/forecast";
 export const LIMIT = 31;
+export const ONE_DAY = 24 * 60 * 60 * 1000;
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { BASE_URL, LIMIT } from "./constants"
+import { BASE_URL, LIMIT, ONE_DAY } from "./constants"
+import { Detail } from "@/types"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -17,4 +18,56 @@ export function absoluteUrl(path: string) {
   return `http://localhost:${
     process.env.PORT ?? 3000
   }${path}`
+}
+
+export function getDifferenceBetweenDates(date1: Date, date2: Date) {
+  date1.setHours(0, 0, 0, 0)
+  date2.setHours(0, 0, 0, 0)
+  return Math.ceil(
+    Math.abs(date1.getTime() - date2.getTime()) / ONE_DAY
+  )
+}
+
+export const generatePredictions = (data: Detail) => {
+  const messages: string[] = []
+
+  if(data.main.temp > 40) {
+    messages.push('It is very hot outside. Carry umbrella and apply sunscream if you are stepping out 🌞.')
+  }
+
+  if(data.wind.speed > 10) {
+    messages.push('It is windy outside. Watch out! 💨.')
+  }
+  
+  const weatherId = data.weather[0].id
+
+  if(weatherId >= 200 && weatherId <= 232) {
+    messages.push('Don’t step out! A Storm is brewing! 🌪️.')
+  }
+
+  if(weatherId >= 300 && weatherId <= 321) {
+    messages.push('Drizzle is expected. Carry umbrella ☔️.')
+  }
+
+  if(weatherId >= 500 && weatherId <= 531) {
+    messages.push('It may rain. Carry Rain coat or unbrella before you go out ☔️.')
+  }
+
+  if(weatherId >= 600 && weatherId <= 622) {
+    messages.push('Carry warm clothes. It\'s snowing outside ❄️.')
+  }
+
+  if(weatherId >= 701 && weatherId <= 781) {
+    messages.push('It is foggy outside. Carry mask and Drive carefully 🌫️.')
+  }
+
+  if(weatherId === 800) {
+    messages.push('Clear sky. Enjoy the day. ✌')
+  }
+
+  if(weatherId >= 801 && weatherId <= 804) {
+    messages.push('It is cloudy outside ☁️.')
+  }
+
+  return messages
 }
