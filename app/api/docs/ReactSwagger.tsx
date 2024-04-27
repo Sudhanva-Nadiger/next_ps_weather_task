7f5d767590a750fc5d3bbeded817ce3e895a3aef
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-04-27 15:32:14 +0530

    add loading ui for swagger api docs page

--- app/api/docs/ReactSwagger.tsx
+++ app/api/docs/ReactSwagger.tsx
@@ -2,10 +2,11 @@
 
 import 'swagger-ui-react/swagger-ui.css'
 import dynamic from "next/dynamic";
+import Loading from '@/app/loading';
 
 const DynamicSwaggerUI = dynamic(() => import("swagger-ui-react"), {
   ssr: false,
-  loading: () => <p>Loading Component...</p>,
+  loading: () => <Loading />,
 });
 
 type Props = {
